b0284eccc8ca2a2c154451133470f6aec4ccd34a
mororazvan04 <morosanu.razvan04@gmail.com>
2026-05-31 15:02:02 +0300

    Add BlocPoveste and Decizie classes to Poveste

--- EvadareBranReader/Story.Core/Models/Poveste.cs
+++ EvadareBranReader/Story.Core/Models/Poveste.cs
@@ -47,3 +47,38 @@ namespace Story.Core.Models
         [JsonPropertyName("onMaxBlock")]
         public string MaxBlock { get; set; }
     }
+    public class BlocPoveste
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+
+        [JsonPropertyName("image")]
+        public string Image { get; set; }
+
+        [JsonPropertyName("isFinal")]
+        public bool IsFinal { get; set; }
+
+        [JsonPropertyName("decisions")]
+        public List<Decizie> Decisions { get; set; } = new List<Decizie>();
+    }
+
+    public class Decizie
+    {
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+
+        [JsonPropertyName("targetBlock")]
+        public string TargetBlock { get; set; }
+
+        [JsonPropertyName("icon")]
+        public string Icon { get; set; }
+
+        [JsonPropertyName("condition")]
+        public Conditie Condition { get; set; }
+
+        [JsonPropertyName("effects")]
+        public List<Efect> Effects { get; set; } = new List<Efect>();
+    }
