12715b18df315124b1d40baa8ed4a9689aad9e40
Bogdy007 <bogdanpanainte424@gmail.com>
2026-06-01 22:25:16 +0300

    Update Form1.Designer.cs

--- EvadareBranEditor/Form1.Designer.cs
+++ EvadareBranEditor/Form1.Designer.cs
@@ -1,20 +1,11 @@
-﻿// ============================================================
-//  Evadare din Castelul Bran — proiect POO (echipă de 4)
-//  AUTOR: Persoana 3 — Aplicația Editor (structura principală) — design fereastră
-// ============================================================
+﻿
 namespace EvadareBranEditor
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
+ 
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -26,10 +17,8 @@ namespace EvadareBranEditor
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
+       
+   
         private void InitializeComponent()
         {
             menuStrip1 = new MenuStrip();
